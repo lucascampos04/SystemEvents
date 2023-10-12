264684114ba8156de07b3f735080ddbef1dee75d
lucascampos04 <camposdlucasoli@gmail.com>
2023-10-12 17:51:54 -0300

    Add materialize

--- src/main/java/com/eventos/eventos/Controller/EventoController.java
+++ src/main/java/com/eventos/eventos/Controller/EventoController.java
@@ -29,6 +29,7 @@ public class EventoController {
     @RequestMapping("/eventos")
     public ModelAndView listaEventos(){
         ModelAndView mv = new ModelAndView("index");
+
         // Buscando eventos
         Iterable<Evento> eventos = er.findAll();
         mv.addObject("eventos", eventos);
