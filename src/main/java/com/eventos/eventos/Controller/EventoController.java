d0065d1ad93f253610bfa3090f63dd1166d001b1
lucascampos04 <camposdlucasoli@gmail.com>
2023-10-12 20:32:35 -0300

    insert de convidados

--- src/main/java/com/eventos/eventos/Controller/EventoController.java
+++ src/main/java/com/eventos/eventos/Controller/EventoController.java
@@ -1,6 +1,8 @@
 package com.eventos.eventos.Controller;
 
+import com.eventos.eventos.Model.Convidado;
 import com.eventos.eventos.Model.Evento;
+import com.eventos.eventos.Repository.ConvidadoReposiroty;
 import com.eventos.eventos.Repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,8 @@ public class EventoController {
     @Autowired
     private EventoRepository er;
 
+    @Autowired
+    private ConvidadoReposiroty cr;
     @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
     public String form(){
         return "Evento/formEvento";
@@ -34,7 +38,7 @@ public class EventoController {
         return mv;
     }
 
-    @RequestMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ModelAndView detalhesEvento(@PathVariable("id") long id){
         Evento evento = er.findById(id);
         ModelAndView mv = new ModelAndView("Evento/detalhesEvento");
@@ -42,5 +46,15 @@ public class EventoController {
         System.out.println("Evento : " + id);
         return mv;
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    public String detalhesEvento(@PathVariable("id") long id, Convidado convidado){
+        Evento evento = er.findById(id);
+        convidado.setEvento(evento);
+        cr.save(convidado);
+        return "redirect:/{id}";
+    }
+
+
 
 }
---
+++ src/main/java/com/eventos/eventos/Repository/ConvidadoReposiroty.java
@@ -0,0 +1,7 @@
+package com.eventos.eventos.Repository;
+
+import com.eventos.eventos.Model.Convidado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConvidadoReposiroty extends CrudRepository<Convidado, String>{
+}
